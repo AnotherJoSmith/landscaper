49312dfbe40cc1746c9acb4f9d2296bc7f66fdf2
AnotherJoSmith <jsmit205@uottawa.ca>
2014-09-28 18:05:51 -0400

    Adding mongodb connection and winston logging

--- app/server.js
+++ app/server.js
@@ -1,16 +1,28 @@
 #!/usr/bin/env node
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var debug = require('debug')('landscaper');
+var mongoose = require('mongoose');
+var log = require("winston").loggers.get("app:server");
 
 var config = require("app/config");
 var middleware = require("app/middleware");
 
 var app = express();
+
+var mongoURL = 'mongodb://' + config.mongodb.host + '/' + config.mongodb.port;
+
+mongoose.connect(mongoURL, function(err, res) {
+  if (err) {
+    log.error('ERROR connecting to: ' + mongoURL + '. ' + err);
+  } else {
+    log.info('Succeeded connected to: ' + mongoURL);
+  }
+});
 
 // view engine setup
 app.set('views', path.join(__dirname, 'site'));
@@ -20,26 +32,28 @@ app.set('view engine', 'hbs');
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 
 [
   "app/site/routes"
-].forEach(function (routePath) {
-    require(routePath)(app);
+].forEach(function(routePath) {
+  require(routePath)(app);
 });
 
 //FINALLY, use any error handlers
 app.use(middleware.notFound);
 
-if(app.get('env') === 'development'){
+if (app.get('env') === 'development') {
   app.use(middleware.devServerError);
 }
 app.use(middleware.prodServerError);
 
-app.listen(config.express.port, config.express.ip, function (error) {
+app.listen(config.express.port, config.express.ip, function(error) {
   if (error) {
     process.exit(10);
   }
-  console.log("Starting server!");
+  log.info("Starting server!");
 });
